c1e306c5c1a84052638e3542e85e15ebc6afaf31
DocNITE <docnite0530@gmail.com>
2022-10-19 11:44:40 +0300

    Bump

--- src/Utils.rb
+++ src/Utils.rb
@@ -13,7 +13,15 @@ def callMsg(fileName)
 end
 
 #for popup player msg
-def callInfoPopup(text)
+def callInfoPopup(fileName)
     SndLib.sound_QuickDialog
-	$game_map.popup(0,text,0,0);
+	case $lang
+        when "ENG"
+            call_msg_popup("../../" + Z_MOD_FOLDER + "text/ENG/" + fileName)
+        when "RUS"
+            #call_msg( "../../" + Z_MOD_FOLDER + "text/RUS/" + fileName)
+            call_msg_popup("../../" + Z_MOD_FOLDER + "text/ENG/" + fileName)
+        else
+            call_msg_popup("../../" + Z_MOD_FOLDER + "text/ENG/" + fileName)
+    end
 end
--- src/event/nap/NapTavern.rb
+++ src/event/nap/NapTavern.rb
@@ -13,3 +13,19 @@
 #               		$game_player.actor.dirt =0
 #               		handleNap
 #-----------------------------------------------------------------------------------------------------
+
+if $game_party.has_item?("3S_ItemTavernKey")
+    $game_party.lose_item($data_ItemName["3S_ItemTavernKey"], 1)
+    $game_player.actor.health =$game_player.actor.attr_dimensions["health"][2]
+    $game_player.actor.sta =$game_player.actor.attr_dimensions["sta"][2]
+    $game_player.actor.sat =$game_player.actor.attr_dimensions["sat"][2]
+    4.times{load_script("Data/Batch/Command_Bath.rb")}
+    $game_player.actor.dirt =0
+    point=$game_map.get_storypoint("WakeUp")
+    standartNapOnPos(point[0], point[1]);
+else
+    callMsg("Tevern_3_roads_main:Tavern/Kicked")
+	portrait_hide
+    #change_map_leave_tag_map
+	change_map_tag_subLevel(Z_TAVERN_STREET_MAP, "StartPoint2", 2, true, true)
+end
