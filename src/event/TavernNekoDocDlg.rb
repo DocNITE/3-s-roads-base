c76f019a8105bb72d48bd5a1666a348ced4c52a3
DocNITE <docnite0530@gmail.com>
2022-10-26 09:25:35 +0300

    asd

---
+++ src/event/TavernNekoDocDlg.rb
@@ -0,0 +1,12 @@
+# Диалог с догниггерским котом
+
+GabeSDK.getAchievement("THIRDS_FirstMeeting");
+
+def cat_t_dlg
+    #callMsg("Tavern_3_roads:Shiza/FirstMeeting");
+end
+cat_t_dlg
+
+$bg.erase;
+cam_center(0);
+$game_temp.choice = -1;
